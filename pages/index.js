802d5696be075d36a04780e637664bf24cea2366
Ayush <tripathyayush123@gmail.com>
2021-12-08 00:43:21 +0530

    initial-commit

---
+++ pages/index.js
@@ -0,0 +1,56 @@
+import Head from "next/head";
+import Feed from "../components/Feed";
+import Sidebar from "../components/Sidebar";
+import Widgets from "../components/Widgets";
+import { getProviders, getSession, useSession } from "next-auth/react";
+import Login from "../components/Login";
+import Modal from "../components/Modal";
+import { modalState } from "../atoms/modalAtom";
+import { useRecoilState } from "recoil";
+
+export default function Home({ trendingResults, followResults, providers }) {
+  const { data: session } = useSession();
+  const [isOpen, setIsOpen] = useRecoilState(modalState);
+
+  if (!session) return <Login providers={providers} />;
+
+  return (
+    <div className="">
+      <Head>
+        <title>Home / Twitter</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className="bg-black min-h-screen flex max-w-[1500px] mx-auto">
+        <Sidebar />
+        <Feed />
+        <Widgets
+          trendingResults={trendingResults}
+          followResults={followResults}
+        />
+
+        {isOpen && <Modal />}
+      </main>
+    </div>
+  );
+}
+
+export async function getServerSideProps(context) {
+  const trendingResults = await fetch("https://jsonkeeper.com/b/NKEV").then(
+    (res) => res.json()
+  );
+  const followResults = await fetch("https://jsonkeeper.com/b/WWMJ").then(
+    (res) => res.json()
+  );
+  const providers = await getProviders();
+  const session = await getSession(context);
+
+  return {
+    props: {
+      trendingResults,
+      followResults,
+      providers,
+      session,
+    },
+  };
+}
